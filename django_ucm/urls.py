151927991eb6aa41107e45d99a0442ea0191455a
Andres Benjamin <andres.benjamin.luna@gmail.com>
2025-06-08 22:17:55 -0400

    update changes for endpoints

--- django_ucm/urls.py
+++ django_ucm/urls.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from django.urls import path
 
 from portal import views
-from portal.views import EvaluationListView
+from portal.views import EvaluationEditView, EvaluationUpdateView, EvaluationByCourseView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('evaluations/', EvaluationListView.as_view(), name='evaluations'),
+    path('evaluations/', EvaluationEditView.as_view(), name='evaluations-edit'),
+    path('evaluations/edit', EvaluationUpdateView.as_view(), name='evaluations-update'),
+    path('evaluations/<str:course_id>/', EvaluationByCourseView.as_view(), name='evaluations-by-course')
+
 ]
 
 
--- portal/views.py
+++ portal/views.py
@@ -1,19 +1,47 @@
 import uuid
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 
+from .models import Evaluation
 from .serializers import EvaluationSerializer
 
 def home(request):
     return render(request, 'home.html')
 
 
-class EvaluationListView(APIView):
+class EvaluationEditView(APIView):
     def post(self, request):
         serializer = EvaluationSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class EvaluationUpdateView(APIView):
+    def put (self, request):
+        data = request.data
+        evaluation_id = data.get('id')
+        if not evaluation_id:
+            return Response({"error": "El campo 'id' es requerido."}, status=status.HTTP_400_BAD_REQUEST)
+
+        evaluation = get_object_or_404(Evaluation, id=evaluation_id)
+        serializer = EvaluationSerializer(evaluation, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class EvaluationByCourseView(APIView):
+    def get(self, request, course_id):
+        evaluations = Evaluation.objects.filter(course_id=course_id)
+        serializer = EvaluationSerializer(evaluations, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+
+
+
+
+
